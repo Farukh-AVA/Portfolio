e6c23c71b46afc5787b51ea420b96c434fc0a062
farukh <farukh022198@gmail.com>
2024-01-04 23:39:18 -0500

    porfolio website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-/*
-	Photon by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
 
 (function($) {
 
@@ -30,3 +25,78 @@
 		$('.scrolly').scrolly();
 
 })(jQuery);
+
+document.addEventListener("DOMContentLoaded", function () {
+	// Show the first three projects
+	showProjects(0, 2);
+	let isShowBtw = true; 
+	// Add click event listener to the "Show More" button
+	document.getElementById("toggleBtn").addEventListener("click", function () {
+		// Toggle the visibility of projects
+		toggleProjects(3, 7);
+		// Toggle the button text
+		isShowBtw = !isShowBtw;  
+		toggleButtonText(isShowBtw);
+	});
+});
+
+function showProjects(startIndex, endIndex) {
+	// Get all project containers
+	var projects = document.querySelectorAll(".project-container .col-4");
+
+	// Loop through the projects and show the ones within the specified range
+	for (var i = 0; i < projects.length; i++) {
+		if (i >= startIndex && i <= endIndex) {
+			projects[i].style.display = "block";
+			projects[i].style.opacity = 1;
+		} else {
+			projects[i].style.display = "none";
+			projects[i].style.opacity = 0;
+		}
+	}
+}
+
+function toggleProjects(startIndex, endIndex) {
+	// Get all project containers
+	var projects = document.querySelectorAll(".project-container .col-4");
+
+	// Loop through the projects and toggle the ones within the specified range
+	for (var i = 0; i < projects.length; i++) {
+		if (i >= startIndex && i <= endIndex) {
+			toggleVisibility(projects[i]);
+		}
+	}
+}
+
+function toggleVisibility(element) {
+	if (element.style.display === "none" || element.style.display === "") {
+		fadeIn(element);
+	} else {
+		hide(element);
+	}
+}
+
+function hide(element) {
+	element.style.display = "none";
+	element.style.opacity = 0;
+}
+
+function fadeIn(element) {
+	element.style.display = "block";
+	element.style.opacity = 0;
+
+	var opacity = 0;
+	var interval = setInterval(function () {
+		if (opacity < 1) {
+			opacity += 0.1;
+			element.style.opacity = opacity;
+		} else {
+			clearInterval(interval);
+		}
+	}, 50); // Adjust the interval as needed
+}
+
+function toggleButtonText(isShowBtw) {
+	var btn = document.getElementById("toggleBtn");
+	btn.innerText = isShowBtw ?"Show More" : "Hide";
+}
